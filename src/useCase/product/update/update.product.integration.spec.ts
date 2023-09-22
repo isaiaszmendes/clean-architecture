88cc12662eb7acc6b3226028a7cea6d103c58796
Isaias Mendes <isaiaszmendes@gmail.com>
2023-09-22 11:41:29 -0300

    add integration test to update a product

---
+++ src/useCase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import { Sequelize } from 'sequelize-typescript';
+import { ProductFactory } from '../../../domain/product/factory/product.factory';
+import { ProductRepository } from '../../../infrastructure/product/repository/sequelize/product.repository';
+import { UpdateProductUseCase } from './update.product.usecase';
+import { ProductModel } from '../../../infrastructure/product/repository/sequelize/product.model';
+
+const product = ProductFactory.create({ name: 'Product 1', price: 10 });
+
+const input = {
+	id: product.id,
+	name: 'Product 2',
+	price: 20,
+};
+
+const output = {
+	id: product.id,
+	name: 'Product 2',
+	price: 20,
+};
+
+
+describe('Integration Test Find Product', () => {
+	let sequelize: Sequelize;
+
+	beforeEach(async () => {
+		sequelize = new Sequelize({
+			dialect: 'sqlite',
+			storage: ':memory:',
+			logging: false,
+			sync: { force: true },
+		});
+
+		sequelize.addModels([ ProductModel ]);
+		await sequelize.sync();
+	});
+
+	afterEach(async() => {
+		await sequelize.close();
+	});
+	it('should find a product', async () => {
+		const productRepository = new ProductRepository();
+		await productRepository.create(product);
+		const useCase = new UpdateProductUseCase(productRepository);
+
+		const result = await useCase.execute(input);
+
+		expect(result).toEqual(output);
+	});
+});
