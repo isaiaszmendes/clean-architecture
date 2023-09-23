ecd40fb8ef551eabb361ca741d230204a08ffbca
Isaias Mendes <isaiaszmendes@gmail.com>
2023-09-23 00:31:27 -0300

    add endpoint to update a customer

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -3,6 +3,7 @@ import { CreateCustomerUseCase } from '../../../useCase/customer/create/create.c
 import { CustomerRepository } from '../../customer/repository/sequelize/customer.repository';
 import { ListCustomerUseCase } from '../../../useCase/customer/list/list.customer.usecase';
 import { FindCustomerUseCase } from '../../../useCase/customer/find/find.customer.usecase';
+import { UpdateCustomerUseCase } from '../../../useCase/customer/update/update.customer.usecase';
 
 export const customerRouter = async (fastify: FastifyInstance) => {
 	fastify.post('/', async (request: FastifyRequest<{
@@ -47,11 +48,45 @@ export const customerRouter = async (fastify: FastifyInstance) => {
 		}
 	});
 
-	fastify.get('/:id', async (request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) => {
+	fastify.get('/:id', async (request: FastifyRequest<{
+		Params: { id: string }
+	}>, reply: FastifyReply) => {
 		const useCase = new FindCustomerUseCase(new CustomerRepository());
 
 		try {
 			const output = await useCase.execute({ id: request.params.id });
+			reply.code(200).send(output);
+		} catch (err) {
+			reply.code(500).send(err);
+		}
+	});
+
+	fastify.put('/:id', async (request: FastifyRequest<{
+		Params: { id: string },
+		Body: {
+			name: string,
+			address: {
+				street: string;
+				number: number;
+				city: string;
+				zip: string;
+			}
+		}
+	}>, reply: FastifyReply) => {
+		const useCase = new UpdateCustomerUseCase(new CustomerRepository());
+
+		try {
+			const requestDTO = {
+				id: request.params.id,
+				name: request.body.name,
+				address: {
+					street: request.body.address.street,
+					number: request.body.address.number,
+					city: request.body.address.city,
+					zip: request.body.address.zip,
+				}
+			};
+			const output = await useCase.execute(requestDTO);
 			reply.code(200).send(output);
 		} catch (err) {
 			reply.code(500).send(err);
