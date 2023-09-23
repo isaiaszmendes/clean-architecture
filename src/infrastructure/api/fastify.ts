91db59999d2adaf5ce534139f19a331aa4c003c1
Isaias Mendes <isaiaszmendes@gmail.com>
2023-09-22 23:52:46 -0300

    map route on fastify file

--- src/infrastructure/api/fastify.ts
+++ src/infrastructure/api/fastify.ts
@@ -2,12 +2,15 @@ import fastify, { FastifyInstance } from 'fastify';
 import { Sequelize } from 'sequelize-typescript';
 import { CustomerModel } from '../customer/repository/sequelize/customer.model';
 import { customerRouter } from './routes/customer.route';
+import { productRouter } from './routes/product.route';
+import { ProductModel } from '../product/repository/sequelize/product.model';
 
 export const app: FastifyInstance = fastify({
 	logger: process.env.NODE_ENV === 'test' ? false : true
 });
 
 app.register(customerRouter, { prefix: '/customer' });
+app.register(productRouter, { prefix: '/product' });
 
 export let sequelize: Sequelize;
 
@@ -17,7 +20,7 @@ async function setupDb() {
 		storage: ':memory:',
 		logging: false,
 	});
-	await sequelize.addModels([ CustomerModel ]);
+	await sequelize.addModels([ CustomerModel, ProductModel ]);
 	await sequelize.sync();
 }
 
