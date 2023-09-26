9698de60989c70de1ec0db52d090672058fb20d5
Isaias Mendes <isaiaszmendes@gmail.com>
2023-09-25 21:55:31 -0300

    add CustomerYupValidator

---
+++ src/domain/customer/validator/customer.yup.validator.ts
@@ -0,0 +1,20 @@
+import * as yup from 'yup';
+import { ValidatorInterface } from '../../@shared/validator/validator.interface';
+import { Customer } from '../entity/customer';
+export class CustomerYupValidator implements ValidatorInterface<Customer> {
+	validator(entity: Customer): void {
+		try {
+			yup
+				.object()
+				.shape({
+					id: yup.string().required('Id is required'),
+					name: yup.string().required('Name is required'),
+				}).validateSync({
+					id: entity.id,
+					name: entity.name,
+				}, { abortEarly: false });
+		} catch (error) {
+		}
+	}
+
+}
